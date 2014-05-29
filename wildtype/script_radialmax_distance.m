228773ff60109b4e1ee968fc21c75278448b4a49
Eeshit Dhaval Vaishnav <eeshit.vaishnav@gmail.com>
2014-05-29 18:48:39 -0400

    Wild type included

---
+++ wildtype/script_radialmax_distance.m
@@ -0,0 +1,29 @@
+
+intersect=intersectLinePolygon(line, t_poly);
+       
+  %%THe plotting of lines takes a long time and is unnecessary now so i am
+  %%commenting this part of the code
+    %if(rok),
+    %       h = fill([cell(cell_index).COM_X intersect(1,1)],[cell(cell_index).COM_Y intersect(1,2)],'r');
+    %       set(h,'FaceColor','None');
+    %       h = fill([cell(cell_index).COM_X intersect(2,1)],[cell(cell_index).COM_Y intersect(2,2)],'r');
+    %       set(h,'FaceColor','None');
+    % end
+     %NOW TO SAVE THE INTENSITIES IN A MATRIX  
+     if(quadrant)
+x=[cell(cell_index).COM_X intersect(1,1)];
+y=[cell(cell_index).COM_Y intersect(1,2)];
+     end
+     
+     if(~quadrant)
+x=[cell(cell_index).COM_X intersect(2,1)];
+y=[cell(cell_index).COM_Y intersect(2,2)];
+     end
+     
+[cx,cy,C_temp]=improfile(A,x,y,'bilinear');
+%%just considering first 30 pixels to reduce abrasion
+if(size(C_temp)>20),
+    C_temp=C_temp(1:20,:);
+end
+[c,i_line]=max(C_temp(:,:));
+MAX=[MAX; cx(i_line) , cy(i_line)];
